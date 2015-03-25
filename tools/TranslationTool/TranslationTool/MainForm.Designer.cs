c7aa03c395272b9bcb6aa26761e68afb1308b306
Namho Kim <namhokim@sindoh.com>
2015-03-25 17:00:31 +0900

    replacement tool +

---
+++ tools/TranslationTool/TranslationTool/MainForm.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace TranslationTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelURL = new System.Windows.Forms.Label();
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.buttonDownload = new System.Windows.Forms.Button();
+            this.progressBarDownload = new System.Windows.Forms.ProgressBar();
+            this.buttonChangeEncoding = new System.Windows.Forms.Button();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.buttonReplacement = new System.Windows.Forms.Button();
+            this.groupBoxCreation = new System.Windows.Forms.GroupBox();
+            this.groupBoxTransformation = new System.Windows.Forms.GroupBox();
+            this.labelNoticeNoReplacementFile = new System.Windows.Forms.Label();
+            this.groupBoxCreation.SuspendLayout();
+            this.groupBoxTransformation.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelURL
+            // 
+            this.labelURL.AutoSize = true;
+            this.labelURL.Location = new System.Drawing.Point(11, 23);
+            this.labelURL.Name = "labelURL";
+            this.labelURL.Size = new System.Drawing.Size(32, 12);
+            this.labelURL.TabIndex = 0;
+            this.labelURL.Text = "URL:";
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxURL.Location = new System.Drawing.Point(47, 20);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(604, 21);
+            this.textBoxURL.TabIndex = 1;
+            this.textBoxURL.TextChanged += new System.EventHandler(this.textBoxURL_TextChanged);
+            // 
+            // buttonDownload
+            // 
+            this.buttonDownload.Enabled = false;
+            this.buttonDownload.Location = new System.Drawing.Point(47, 47);
+            this.buttonDownload.Name = "buttonDownload";
+            this.buttonDownload.Size = new System.Drawing.Size(75, 23);
+            this.buttonDownload.TabIndex = 2;
+            this.buttonDownload.Text = "&Download";
+            this.buttonDownload.UseVisualStyleBackColor = true;
+            this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+            // 
+            // progressBarDownload
+            // 
+            this.progressBarDownload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBarDownload.Location = new System.Drawing.Point(62, 233);
+            this.progressBarDownload.Name = "progressBarDownload";
+            this.progressBarDownload.Size = new System.Drawing.Size(594, 21);
+            this.progressBarDownload.TabIndex = 3;
+            this.progressBarDownload.Visible = false;
+            // 
+            // buttonChangeEncoding
+            // 
+            this.buttonChangeEncoding.Enabled = false;
+            this.buttonChangeEncoding.Location = new System.Drawing.Point(53, 59);
+            this.buttonChangeEncoding.Name = "buttonChangeEncoding";
+            this.buttonChangeEncoding.Size = new System.Drawing.Size(234, 23);
+            this.buttonChangeEncoding.TabIndex = 4;
+            this.buttonChangeEncoding.Text = "&Change Encoding & Remove script";
+            this.buttonChangeEncoding.UseVisualStyleBackColor = true;
+            this.buttonChangeEncoding.Click += new System.EventHandler(this.buttonChangeEncoding_Click);
+            // 
+            // labelFile
+            // 
+            this.labelFile.AutoSize = true;
+            this.labelFile.Location = new System.Drawing.Point(11, 34);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(29, 12);
+            this.labelFile.TabIndex = 5;
+            this.labelFile.Text = "File:";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.AllowDrop = true;
+            this.textBoxFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFile.Location = new System.Drawing.Point(47, 32);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(604, 21);
+            this.textBoxFile.TabIndex = 6;
+            this.textBoxFile.TextChanged += new System.EventHandler(this.textBoxFile_TextChanged);
+            this.textBoxFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxFile_DragDrop);
+            this.textBoxFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxFile_DragEnter);
+            // 
+            // buttonReplacement
+            // 
+            this.buttonReplacement.Enabled = false;
+            this.buttonReplacement.Location = new System.Drawing.Point(302, 59);
+            this.buttonReplacement.Name = "buttonReplacement";
+            this.buttonReplacement.Size = new System.Drawing.Size(111, 23);
+            this.buttonReplacement.TabIndex = 7;
+            this.buttonReplacement.Text = "&Replacement";
+            this.buttonReplacement.UseVisualStyleBackColor = true;
+            this.buttonReplacement.Click += new System.EventHandler(this.buttonReplacement_Click);
+            // 
+            // groupBoxCreation
+            // 
+            this.groupBoxCreation.Controls.Add(this.textBoxURL);
+            this.groupBoxCreation.Controls.Add(this.labelURL);
+            this.groupBoxCreation.Controls.Add(this.buttonDownload);
+            this.groupBoxCreation.Location = new System.Drawing.Point(15, 12);
+            this.groupBoxCreation.Name = "groupBoxCreation";
+            this.groupBoxCreation.Size = new System.Drawing.Size(657, 80);
+            this.groupBoxCreation.TabIndex = 8;
+            this.groupBoxCreation.TabStop = false;
+            this.groupBoxCreation.Text = "Creation";
+            // 
+            // groupBoxTransformation
+            // 
+            this.groupBoxTransformation.Controls.Add(this.labelNoticeNoReplacementFile);
+            this.groupBoxTransformation.Controls.Add(this.textBoxFile);
+            this.groupBoxTransformation.Controls.Add(this.buttonChangeEncoding);
+            this.groupBoxTransformation.Controls.Add(this.buttonReplacement);
+            this.groupBoxTransformation.Controls.Add(this.labelFile);
+            this.groupBoxTransformation.Location = new System.Drawing.Point(15, 110);
+            this.groupBoxTransformation.Name = "groupBoxTransformation";
+            this.groupBoxTransformation.Size = new System.Drawing.Size(657, 100);
+            this.groupBoxTransformation.TabIndex = 9;
+            this.groupBoxTransformation.TabStop = false;
+            this.groupBoxTransformation.Text = "Transformation";
+            // 
+            // labelNoticeNoReplacementFile
+            // 
+            this.labelNoticeNoReplacementFile.AutoSize = true;
+            this.labelNoticeNoReplacementFile.ForeColor = System.Drawing.Color.Red;
+            this.labelNoticeNoReplacementFile.Location = new System.Drawing.Point(437, 64);
+            this.labelNoticeNoReplacementFile.Name = "labelNoticeNoReplacementFile";
+            this.labelNoticeNoReplacementFile.Size = new System.Drawing.Size(115, 12);
+            this.labelNoticeNoReplacementFile.TabIndex = 8;
+            this.labelNoticeNoReplacementFile.Text = "No replacement file";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 295);
+            this.Controls.Add(this.groupBoxTransformation);
+            this.Controls.Add(this.groupBoxCreation);
+            this.Controls.Add(this.progressBarDownload);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Translation Tool";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBoxCreation.ResumeLayout(false);
+            this.groupBoxCreation.PerformLayout();
+            this.groupBoxTransformation.ResumeLayout(false);
+            this.groupBoxTransformation.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelURL;
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button buttonDownload;
+        private System.Windows.Forms.ProgressBar progressBarDownload;
+        private System.Windows.Forms.Button buttonChangeEncoding;
+        private System.Windows.Forms.Label labelFile;
+        private System.Windows.Forms.TextBox textBoxFile;
+        private System.Windows.Forms.Button buttonReplacement;
+        private System.Windows.Forms.GroupBox groupBoxCreation;
+        private System.Windows.Forms.GroupBox groupBoxTransformation;
+        private System.Windows.Forms.Label labelNoticeNoReplacementFile;
+    }
+}
+
---
+++ tools/TranslationTool/TranslationTool/MainForm.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TranslationTool
+{
+    public partial class MainForm : Form
+    {
+        #region "Constants"
+        private const string ReplaceRuleFile = "rrf.txt";
+        private static readonly Encoding ISO_8859_1 = Encoding.GetEncoding("ISO-8859-1");
+        private static readonly Encoding Utf8WithoutBOM = new System.Text.UTF8Encoding(false);
+        #endregion
+
+        #region "Private variation"
+        private Dictionary<string, string> dic = new Dictionary<string, string>();
+        #endregion
+
+        #region "MainForm"
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            labelNoticeNoReplacementFile.Visible = (HasRuleFile == false);
+        }
+
+        private bool HasRuleFile
+        {
+            get
+            {
+                return File.Exists(ReplaceRuleFile);
+            }
+        }
+        #endregion
+
+        #region "textBoxURL"
+        private void textBoxURL_TextChanged(object sender, EventArgs e)
+        {
+            buttonDownload.Enabled = Uri.IsWellFormedUriString(textBoxURL.Text, UriKind.Absolute);
+        }
+        #endregion
+
+        #region "buttonDownload"
+        private void buttonDownload_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri(textBoxURL.Text);
+            String filename = Path.GetFileName(uri.LocalPath);
+            if (filename.Length == 0) filename = "index.html";
+
+            if (File.Exists(filename))
+            {
+                DialogResult reply = MessageBox.Show(
+                    "File already exist. Overwrite it?", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (reply == DialogResult.No) return;
+            }
+
+            buttonDownload.Enabled = false;
+            progressBarDownload.Visible = true;
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+            webClient.DownloadFileAsync(uri, filename, filename);
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBarDownload.Value = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            textBoxFile.Text = e.UserState as string;
+        }
+        #endregion
+
+        #region "textBoxFile"
+        private void textBoxFile_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsContentUTF8(textBoxFile.Text))
+                {
+                    buttonChangeEncoding.Enabled = false;
+                    buttonReplacement.Enabled = true;
+                }
+                else
+                {
+                    buttonChangeEncoding.Enabled = true;
+                    buttonReplacement.Enabled = false;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                buttonChangeEncoding.Enabled = false;
+                buttonReplacement.Enabled = false;
+            }
+        }
+
+        private bool IsContentUTF8(string filename)
+        {
+            if (File.Exists(filename) == false)
+            {
+                throw new FileNotFoundException();
+            }
+
+            using (var sr = new StreamReader(filename, Utf8WithoutBOM, false))
+            {
+                while (sr.Peek() > 0)
+                {
+                    string line = sr.ReadLine();
+                    if (line.Contains("charset=UTF-8")) return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool HasUTF8_BOM(string filename)
+        {
+            if (File.Exists(filename) == false)
+            {
+                throw new FileNotFoundException();
+            }
+
+            using (var file = new FileStream(filename, FileMode.Open))
+            {
+                byte[] bom = new byte[3];
+                if (file.Read(bom, 0, 3) != 3) return false;
+                return (bom[0] == 0xEF && bom[1] == 0xBB && bom[2] == 0xBF);
+            }
+        }
+
+        private void textBoxFile_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (!Directory.Exists(file))
+                {
+                    textBoxFile.Text = file;
+                    break;
+                }
+            }
+        }
+
+        private void textBoxFile_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+        #endregion
+
+        #region "buttonChangeEncoding"
+        private void buttonChangeEncoding_Click(object sender, EventArgs e)
+        {
+            buttonChangeEncoding.Enabled = false;
+            progressBarDownload.Visible = true;
+            progressBarDownload.Value = 0;
+
+            string ori = textBoxFile.Text;
+            string temp = Guid.NewGuid().ToString();
+            PreProcessing(ori, temp);
+            File.Replace(temp, ori, null);
+
+            buttonReplacement.Enabled = true;
+            progressBarDownload.Value = 100;
+        }
+
+        /// <summary>
+        /// 1. 인코딩 변경
+        /// 2. 인코딩 meta 엘리먼트 값 변경
+        /// 3. 끝 부분 script 부분 제거
+        /// </summary>
+        /// <param name="originalFilename">입력 파일</param>
+        /// <param name="saveToFilename">출력 파일</param>
+        private void PreProcessing(string originalFilename, string saveToFilename)
+        {
+            const string MetaOrig = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=ISO-8859-1\">";
+            const string MetaDest = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">";
+            const string BeginScript = "<script type=\"text/javascript\">";
+            const string EndScript = "</script>";
+
+            long totalSize = GetFileSize(originalFilename);
+            long read = 0;
+            int scriptLevel = 0;
+            using (var sr = new StreamReader(originalFilename, ISO_8859_1, false))
+            using (var sw = new StreamWriter(saveToFilename, false, Utf8WithoutBOM))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    read += line.Length;
+                    progressBarDownload.Value = (int)(read * 100 / totalSize);
+
+                    if (line == MetaOrig)
+                    {
+                        sw.WriteLine(MetaDest);
+                        continue;
+                    }
+                    else if (line == BeginScript)
+                    {
+                        scriptLevel++;
+                    }
+                    else if (line == EndScript)
+                    {
+                        if (scriptLevel == 2)
+                        {
+                            scriptLevel = 0;
+                            continue;   // ignore for write "</script>"
+                        }
+                    }
+
+                    if (scriptLevel == 0)
+                    {
+                        if (sr.Peek() >= 0) sw.WriteLine(line);
+                        else sw.Write(line);
+                    }
+                }
+            }
+        }
+
+        private long GetFileSize(string originalFilename)
+        {
+            FileInfo fi = new FileInfo(originalFilename);
+            return fi.Length;
+        }
+        #endregion
+
+        #region "buttonReplacement"
+        private void buttonReplacement_Click(object sender, EventArgs e)
+        {
+            buttonReplacement.Enabled = false;
+            progressBarDownload.Visible = true;
+            progressBarDownload.Value = 0;
+
+            if (!HasRuleFile)
+            {
+                DialogResult reply = MessageBox.Show(
+                    "There is no replacement rule file.\r\nDo you want create it?",
+                    "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (reply == DialogResult.No) return;
+                MakeNewReplacementFile();
+                OpenReplacementFile();
+            }
+            LoadReplacementDataFromFile();
+
+            string ori = textBoxFile.Text;
+            string temp = Guid.NewGuid().ToString();
+            Replacement(ori, temp);
+            File.Replace(temp, ori, null);
+
+            progressBarDownload.Value = 100;
+        }
+
+        private void MakeNewReplacementFile()
+        {
+            using (StreamWriter sw = File.CreateText(ReplaceRuleFile))
+            {
+                sw.WriteLine("Replacement target 1");
+                sw.WriteLine("교체될 값 1");
+                sw.WriteLine();
+                sw.WriteLine("Replacement target 2");
+                sw.WriteLine("교체될 값 2");
+            }
+        }
+
+        private void OpenReplacementFile()
+        {
+            Process process = Process.Start(ReplaceRuleFile);
+            process.WaitForExit();
+        }
+
+        private void LoadReplacementDataFromFile()
+        {
+            using (StreamReader sr = File.OpenText(ReplaceRuleFile))
+            {
+                while (sr.Peek() > 0)
+                {
+                    string line1 = sr.ReadLine();
+                    if (line1.Length == 0) continue;    // if not using 2 1(empty) 2 1(empty) pattern
+
+                    if (sr.Peek() > 0)
+                    {
+                        string line2 = sr.ReadLine();
+                        dic.Add(line1, line2);
+                    }
+                }
+            }
+        }
+
+        private void Replacement(string originalFilename, string saveToFilename)
+        {
+            var keys = dic.Keys;
+            if (keys.Count == 0) return;
+
+            long totalSize = GetFileSize(originalFilename);
+            long read = 0;
+            using (var sr = new StreamReader(originalFilename, Utf8WithoutBOM, false))
+            using (var sw = new StreamWriter(saveToFilename, false, Utf8WithoutBOM))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    read += line.Length;
+                    progressBarDownload.Value = (int)(read * 100 / totalSize);
+
+                    foreach (string k in keys)
+                    {
+                        line = line.Replace(k, dic[k]);
+                    }
+
+                    if (sr.Peek() >= 0) sw.WriteLine(line);
+                    else sw.Write(line);
+                }
+            }
+        }
+        #endregion
+    }
+}
